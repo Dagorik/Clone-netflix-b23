f824602c9fb05ccf0cbfc647d4ba83ca22a46d0d
LigorioSalgado <salgadolg5@hotmail.com>
2018-10-02 21:50:58 -0500

    paypal integration

--- src/components/Me/Me.js
+++ src/components/Me/Me.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import {Query} from  'react-apollo';
 import FormME from './FormME';
+import Paypal from '../Paypal/Paypal';
 
 const GET_ME = gql`
 
@@ -21,11 +22,26 @@ const GET_ME = gql`
     }
 `;
 
+const CLIENT =  {
+    sandbox:"AR3IGrHtCd_eon-Hj-77ujv4ofklvBCuMoEUOb4n8QrAuugzpcIFEDmoNnbjYQP8vld0tzbac0_SfV5V",
+    production:""
+
+}
+
+const ENV = process.env.NODE_ENV === "production" ? "production" : "sandbox"
+
+const onSuccess =  (payment) => { console.log(payment)}
+
+const onError =  (error) => console.log("Error:",error)
+
+const onCancel =  (data) => console.log("Cancel",data)
 
 class Me extends Component{
+
     render(){
         return(
-            <Query query={GET_ME}>
+            <div>
+                <Query query={GET_ME}>
                 {
                     ({loading,error,data}) => {
                         if(loading) return ( <h4>Loading...</h4> )
@@ -36,7 +52,24 @@ class Me extends Component{
 
                 }
 
-            </Query>
+                </Query>
+
+
+                <Paypal
+                    client={CLIENT}
+                    env={ENV}
+                    commit={true}
+                    currency={'MXN'}
+                    total={100}
+                    onSuccess={onSuccess}
+                    onError={onError}
+                    onCancel={onCancel}
+                
+                />
+
+
+            </div>
+            
 
         )
     }
